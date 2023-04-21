326cdd8b8c47d025921b7d758b110ed8e580ef44
chunhui_true <chunhui_true@163.com>
2023-04-21 09:56:02 +0800

    打开dap mode

--- init.el
+++ init.el
@@ -412,7 +412,7 @@ when it inserts comment at the end of the line. "
               ("C-c C-c s" . lsp-rust-analyzer-status)
               ("C-c C-c e" . lsp-rust-analyzer-expand-macro)
 
-              ;; ("C-c C-c H" . dap-hydra)
+              ("C-c C-c h" . dap-hydra)
               ;; ("C-c C-c a" . lsp-execute-code-action)
               ;; ("C-c C-c q" . lsp-workspace-restart)
               ;; ("C-c C-c Q" . lsp-workspace-shutdown)
@@ -459,31 +459,31 @@ when it inserts comment at the end of the line. "
 ;; (add-hook 'c-mode-hook 'helm-gtags-mode)
 
 
-;; ;; setting up debugging support with dap-mode
-;; (use-package exec-path-from-shell
-;;   :ensure
-;;   :init (exec-path-from-shell-initialize))
+;; dap-mode
+(use-package exec-path-from-shell
+  :ensure
+  :init (exec-path-from-shell-initialize))
 
-;; (when (executable-find "lldb-mi")
-;;   (use-package dap-mode
-;;     :ensure
-;;     :config
-;;     (dap-ui-mode)
-;;     (dap-ui-controls-mode 1)
+(when (executable-find "lldb-mi")
+  (use-package dap-mode
+    :ensure
+    :config
+    (dap-ui-mode)
+    (dap-ui-controls-mode 1)
 
-;;     (require 'dap-lldb)
-;;     (require 'dap-gdb-lldb)
-;;     ;; installs .extension/vscode
-;;     (dap-gdb-lldb-setup)
-;;     (dap-register-debug-template
-;;      "Rust::LLDB Run Configuration"
-;;      (list :type "lldb"
-;;            :request "launch"
-;;            :name "LLDB::Run"
-;; 	       :gdbpath "rust-lldb"
-;;            ;; uncomment if lldb-mi is not in PATH
-;;            ;; :lldbmipath "path/to/lldb-mi"
-;;            ))))
+    (require 'dap-lldb)
+    (require 'dap-gdb-lldb)
+    ;; installs .extension/vscode
+    (dap-gdb-lldb-setup)
+    (dap-register-debug-template
+     "Rust::LLDB Run Configuration"
+     (list :type "lldb"
+           :request "launch"
+           :name "LLDB::Run"
+	       :gdbpath "rust-lldb"
+           ;; uncomment if lldb-mi is not in PATH
+           ;; :lldbmipath "path/to/lldb-mi"
+           ))))
 
 
 ;; https://emacs-lsp.github.io/lsp-mode/page/lsp-lua-lsp/
