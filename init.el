2525972c4da24760625d359d3c59d401f25b02bb
lch <chunhui_true@163.com>
2023-08-10 18:06:19 +0800

    x

--- init.el
+++ init.el
@@ -504,35 +504,37 @@ when it inserts comment at the end of the line. "
 ;; (add-hook 'c-mode-hook 'helm-gtags-mode)
 
 
-;; ;; dap-mode
-;; ;; https://robert.kra.hn/posts/rust-emacs-setup/  https://www.jianshu.com/p/73ad87d0b88b 中文
-;; (use-package exec-path-from-shell
-;;   :ensure
-;;   :init (exec-path-from-shell-initialize))
+;; dap-mode 需要此包
+(use-package exec-path-from-shell
+  :ensure
+  :init (exec-path-from-shell-initialize))
 
-;; (when (executable-find "lldb-mi")
-;;   (use-package dap-mode
-;;     :ensure
-;;     :bind (:map dap-mode
-;;                 ("C-c C-c h" . dap-hydra)
-;;                 )
-;;     :config
-;;     (dap-ui-mode)
-;;     (dap-ui-controls-mode 1)
 
-;;     (require 'dap-lldb)
-;;     (require 'dap-gdb-lldb)
-;;     ;; installs .extension/vscode
-;;     (dap-gdb-lldb-setup)
-;;     (dap-register-debug-template
-;;      "Rust::LLDB Run Configuration"
-;;      (list :type "lldb"
-;;            :request "launch"
-;;            :name "LLDB::Run"
-;; 	       :gdbpath "rust-lldb"
-;;            ;; uncomment if lldb-mi is not in PATH
-;;            ;; :lldbmipath "path/to/lldb-mi"
-;;            ))))
+;; dap-mode
+;; https://robert.kra.hn/posts/rust-emacs-setup/  https://www.jianshu.com/p/73ad87d0b88b 中文
+(when (executable-find "lldb-mi")
+  (use-package dap-mode
+    :ensure
+    :bind (:map dap-mode
+                ("C-c C-c h" . dap-hydra)
+                )
+    :config
+    (dap-ui-mode)
+    (dap-ui-controls-mode 1)
+
+    (require 'dap-lldb)
+    (require 'dap-gdb-lldb)
+    ;; installs .extension/vscode
+    (dap-gdb-lldb-setup)
+    (dap-register-debug-template
+     "Rust::LLDB Run Configuration"
+     (list :type "lldb"
+           :request "launch"
+           :name "LLDB::Run"
+	       :gdbpath "rust-lldb"
+           ;; uncomment if lldb-mi is not in PATH
+           ;; :lldbmipath "path/to/lldb-mi"
+           ))))
 
 
 ;; ssh中emacs copy的内容放到系统剪切板
