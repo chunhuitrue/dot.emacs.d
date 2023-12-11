c24c783791c2a52470285eec8a47d6a998133784
chunhui_true <chunhui_true@163.com>
2023-12-11 09:49:50 +0800

    去掉自动保存

--- init.el
+++ init.el
@@ -87,18 +87,18 @@
 ;; (setq auto-save-default nil)
 ;; (setq undo-tree-auto-save-history nil)
 
-(add-to-list 'load-path "~/.emacs.d/site-lisp/auto-save-master")
-(require 'auto-save)
-(auto-save-enable)
-(setq auto-save-silent t)   ; quietly save
-;; (setq auto-save-delete-trailing-whitespace t)  ; automatically delete spaces at the end of the line when saving
-;; ;;; custom predicates if you don't want auto save.
-;; ;;; disable auto save mode when current filetype is an gpg file.
-;;   (setq auto-save-disable-predicates
-;; 	'((lambda ()
-;; 	    (string-suffix-p
-;; 	     "gpg"
-;; 	     (file-name-extension (buffer-name)) t))))
+;; (add-to-list 'load-path "~/.emacs.d/site-lisp/auto-save-master")
+;; (require 'auto-save)
+;; (auto-save-enable)
+;; (setq auto-save-silent t)   ; quietly save
+;; ;; (setq auto-save-delete-trailing-whitespace t)  ; automatically delete spaces at the end of the line when saving
+;; ;; ;;; custom predicates if you don't want auto save.
+;; ;; ;;; disable auto save mode when current filetype is an gpg file.
+;; ;;   (setq auto-save-disable-predicates
+;; ;; 	'((lambda ()
+;; ;; 	    (string-suffix-p
+;; ;; 	     "gpg"
+;; ;; 	     (file-name-extension (buffer-name)) t))))
 
 
 ;; 自动备份
