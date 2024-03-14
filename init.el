8c598ddc1e634c75c31510cef02fb403af76ee3d
chunhui_true <chunhui_true@163.com>
2024-03-14 17:39:49 +0800

    删掉ws-butler的其他配置方式

--- init.el
+++ init.el
@@ -845,12 +845,3 @@ when it inserts comment at the end of the line. "
           (not (eq 'font-lock-string-face
                    (get-text-property end 'face)))))
   )
-
-;; (require 'ws-butler)
-;; (add-hook 'prog-mode-hook #'ws-butler-mode)
-;; (setq ws-butler-global-exempt-modes '(special-mode))
-;; (setq ws-butler-keep-whitespace-before-point nil)
-;; (setq ws-butler-trim-predicate
-;;       (lambda (beg end)
-;;         (not (eq 'font-lock-string-face
-;;                  (get-text-property end 'face)))))
