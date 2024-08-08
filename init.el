7496cb166296fb1abbec72921d8f765c3ce7ba46
chunhui_true <chunhui_true@163.com>
2024-08-08 09:59:39 +0800

    添加dogears

--- init.el
+++ init.el
@@ -944,3 +944,31 @@ when it inserts comment at the end of the line. "
   ;; (setq vundo-glyph-alist vundo-unicode-symbols)
   (set-face-attribute 'vundo-default nil :family "Symbola")
   )
+
+
+(use-package dogears
+  :ensure t
+  ;; :straight t
+  :hook (after-init . dogears-mode)
+  :bind (:map global-map
+              ("M-g d" . dogears-go)
+              ("M-g M-b" . dogears-back)
+              ("M-g M-f" . dogears-forward)
+              ("M-g M-d" . dogears-list)
+              ("M-g M-D" . dogears-sidebar))
+  :config
+  (setq dogears-idle 1
+        dogears-limit 200
+        dogears-position-delta 20)
+  (setq dogears-functions '(find-file recenter-top-bottom
+                                      other-window switch-to-buffer
+                                      aw-select toggle-window-split
+                                      windmove-do-window-select
+                                      pager-page-down pager-page-up
+                                      tab-bar-select-tab
+                                      pop-to-mark-command
+                                      pop-global-mark
+                                      goto-last-change
+                                      xref-go-back
+                                      xref-find-definitions
+                                      xref-find-references)))
