f5aea95c62b6002524404077d95472cd1c540fac
chunhui_true <chunhui_true@163.com>
2025-01-17 22:25:23 +0800

    x

---
+++ elpa/helm-core-20250116.1339/helm-core-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "helm-core" "20250116.1339"
+  "Development files for Helm."
+  '((emacs "25.1")
+    (async "1.9.8"))
+  :url "https://emacs-helm.github.io/helm/"
+  :commit "94aaa3e99c49ac40eddd8d3fff744b6f5a1e6113"
+  :revdesc "94aaa3e99c49"
+  :authors '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers '(("Thierry Volpiatto" . "thievol@posteo.net")))
---
+++ elpa/magit-20250116.1243/magit-pkg.el
@@ -0,0 +1,18 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit" "20250116.1243"
+  "A Git porcelain inside Emacs."
+  '((emacs         "27.1")
+    (compat        "30.0.1.0")
+    (dash          "2.19.1")
+    (magit-section "4.2.0")
+    (seq           "2.24")
+    (transient     "0.8.2")
+    (with-editor   "3.4.3"))
+  :url "https://github.com/magit/magit"
+  :commit "ca5b77284adf8d9af3f98eb669bd47bbf24c46f6"
+  :revdesc "ca5b77284adf"
+  :keywords '("git" "tools" "vc")
+  :authors '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+             ("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")
+                 ("Kyle Meyer" . "kyle@kyleam.com")))
